ab759a80fc55d5287fc6f28b23b5ba5f59e1cc1f
Carolyn Huynh & Justin Pacubas <student@epicodus.com>
2014-09-18 16:24:25 -0700

    Made it supa cute

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,54 @@
+class TweetsController < ApplicationController
+
+  def index
+    @user = User.find(params[:user_id])
+    @tweets = @user.tweets.all.reverse
+    @tweet = Tweet.new
+  end
+
+  def new
+    @user = User.find(params[:user_id])
+    @tweet = Tweet.new
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+        respond_to do |format|
+        format.html { redirect_to user_tweet_path(@tweet) }
+        format.js
+      end
+    end
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to user_tweet_path(@tweet)
+    else
+      redirect_to :back, alert: "Try again, scumbag."
+    end
+  end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to user_tweet_path, notice: "Tweet deleted, scumbag."
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content).merge(user_id: current_user.id)
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,52 @@
+class UsersController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      UserMailer.signup_confirmation(@user).deliver
+      session[:user_id] = @user.id
+      redirect_to user_tweets_path(@user), notice: "Thanks, scumbag."
+    else
+      redirect_to :back, alert: "Try again, scumbag."
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      redirect_to :back, alert: "Try again, scumbag."
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Bye bye, scumbag."
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_confirmation, :password)
+  end
+
+end
